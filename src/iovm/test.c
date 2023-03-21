bc3210c8d87511a6843d7ba902748a7d31ac9941
jsd1982 <james.jdunne@gmail.com>
2023-03-21 11:47:46 -0500

    iovm: added test for write snescmd

--- src/iovm/test.c
+++ src/iovm/test.c
@@ -226,7 +226,7 @@ int test_read_non_repeat_non_immed_sram() {
     VERIFY_EQ_INT(1, fake_iovm1_emit.count, "iovm1_emit() invocations");
 
     // verify expected behavior:
-    VERIFY_EQ_INT((int) 1, (int) fake_target[target].address, "address");
+    VERIFY_EQ_INT(1, (int) fake_target[target].address, "address");
     VERIFY_EQ_INT((int) fake_target[target].expected_read, (int) fake_target[target].last_read, "byte read");
     VERIFY_EQ_INT((int) fake_last_emitted, (int) fake_target[target].expected_read, "byte emitted");
 
@@ -278,8 +278,8 @@ int test_read_non_repeat_non_immed_snescmd() {
     VERIFY_EQ_INT(1, fake_iovm1_emit.count, "iovm1_emit() invocations");
 
     // verify expected behavior:
-    VERIFY_EQ_INT((int) 1, (int) fake_target[target].address, "address");
-    VERIFY_EQ_INT((int) 0x55, (int) fake_target[target].last_read, "byte read");
+    VERIFY_EQ_INT(1, (int) fake_target[target].address, "address");
+    VERIFY_EQ_INT(0x55, (int) fake_target[target].last_read, "byte read");
     VERIFY_EQ_INT((int) fake_last_emitted, (int) fake_target[target].expected_read, "byte emitted");
 
     // should end:
@@ -331,8 +331,61 @@ int test_write_non_repeat_immed_sram() {
     VERIFY_EQ_INT(0, fake_iovm1_emit.count, "iovm1_emit() invocations");
 
     // verify expected behavior:
-    VERIFY_EQ_INT((int) 1, (int) fake_target[target].address, "address");
-    VERIFY_EQ_INT((int) 0x99, (int) fake_target[target].last_write, "byte written");
+    VERIFY_EQ_INT(1, (int) fake_target[target].address, "address");
+    VERIFY_EQ_INT(0x99, (int) fake_target[target].last_write, "byte written");
+    VERIFY_EQ_INT((int) fake_last_emitted, (int) fake_target[target].expected_read, "byte emitted");
+
+    // should end:
+    r = iovm1_exec_step(&vm);
+    VERIFY_EQ_INT(0, r, "iovm1_exec_step() return value");
+    VERIFY_EQ_INT(IOVM1_STATE_ENDED, iovm1_state(&vm), "state");
+
+    return 0;
+}
+
+int test_write_non_repeat_immed_snescmd() {
+    int r;
+    struct iovm1_t vm;
+    int target = IOVM1_TARGET_SNESCMD;
+    uint8_t prgm[] = {
+        IOVM1_MKINST(IOVM1_OPCODE_WRITE, 1, 0, 1, target),
+        0xBB,
+        0
+    };
+
+    r = iovm1_load(&vm, sizeof(prgm), prgm);
+    VERIFY_EQ_INT(0, r, "iovm1_load() return value");
+    VERIFY_EQ_INT(IOVM1_STATE_LOADED, iovm1_state(&vm), "state");
+
+    // first execution initializes registers:
+    r = iovm1_exec_step(&vm);
+    VERIFY_EQ_INT(0, r, "iovm1_exec_step() return value");
+    VERIFY_EQ_INT(IOVM1_STATE_EXECUTING, iovm1_state(&vm), "state");
+
+    // entered WRITING state:
+    r = iovm1_exec_step(&vm);
+    VERIFY_EQ_INT(0, r, "iovm1_exec_step() return value");
+    VERIFY_EQ_INT(IOVM1_STATE_WRITING, iovm1_state(&vm), "state");
+
+    // performs READ:
+    r = iovm1_exec_step(&vm);
+    VERIFY_EQ_INT(0, r, "iovm1_exec_step() return value");
+    VERIFY_EQ_INT(IOVM1_STATE_EXECUTING, iovm1_state(&vm), "state");
+
+    // verify invocations:
+    VERIFY_EQ_INT(0, fake_iovm1_target_set_address.count, "iovm1_target_set_address() invocations");
+    VERIFY_EQ_INT(0, fake_iovm1_target_read.count, "iovm1_target_read() invocations");
+
+    VERIFY_EQ_INT(1, fake_iovm1_target_write.count, "iovm1_target_write() invocations");
+    VERIFY_EQ_INT(target, fake_iovm1_target_write.target, "iovm1_target_write(_, target, _, _)");
+    VERIFY_EQ_INT(1, fake_iovm1_target_write.advance, "iovm1_target_write(_, _, advance, _)");
+    VERIFY_EQ_INT(0xBB, (int)fake_iovm1_target_write.data, "iovm1_target_write(_, _, _, data)");
+
+    VERIFY_EQ_INT(0, fake_iovm1_emit.count, "iovm1_emit() invocations");
+
+    // verify expected behavior:
+    VERIFY_EQ_INT(1, (int) fake_target[target].address, "address");
+    VERIFY_EQ_INT(0xBB, (int) fake_target[target].last_write, "byte written");
     VERIFY_EQ_INT((int) fake_last_emitted, (int) fake_target[target].expected_read, "byte emitted");
 
     // should end:
@@ -368,6 +421,7 @@ int run_test_suite() {
     run_test(test_read_non_repeat_non_immed_sram)
     run_test(test_read_non_repeat_non_immed_snescmd)
     run_test(test_write_non_repeat_immed_sram)
+    run_test(test_write_non_repeat_immed_snescmd)
 
     return 0;
 }
